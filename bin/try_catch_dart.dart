ddd8c18049da44c04e785c41158f7f089b11e2d0
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-07 11:30:15 -0600

    updt: try catch class

--- bin/try_catch_dart.dart
+++ bin/try_catch_dart.dart
@@ -1,5 +1,18 @@
-import 'package:try_catch_dart/try_catch_dart.dart' as try_catch_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${try_catch_dart.calculate()}!');
+void displayName(str) {
+  if (str.length > 0) {
+    print(str);
+  } else {
+    throw Exception('Name is empty.');
+  }
+}
+ 
+void main() {
+  var name = '';
+  try {
+    displayName(name);
+  } catch (e) {
+    print('There is an exception.');
+  }
+  //* finally
+  print('Bye');
 }
