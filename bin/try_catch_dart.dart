a6f3da58ff9d752fe8c63d01682ac082715e7380
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-07 11:15:33 -0600

    Initial commit

---
+++ bin/try_catch_dart.dart
@@ -0,0 +1,5 @@
+import 'package:try_catch_dart/try_catch_dart.dart' as try_catch_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${try_catch_dart.calculate()}!');
+}
---
+++ test/try_catch_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:try_catch_dart/try_catch_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
